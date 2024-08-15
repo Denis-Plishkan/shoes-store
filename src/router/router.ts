a780950caf83231438bee644ee303181c9524a77
Denis-Plishkan <frontend.plishkan@gmail.com>
2024-08-15 12:16:53 +0300

    'create'

---
+++ src/router/router.ts
@@ -0,0 +1,15 @@
+import {createRouter, createWebHashHistory} from "vue-router";
+import MainPage from "../views/MainPage.vue";
+
+const router = createRouter({
+    history: createWebHashHistory(import.meta.env.BASE_URL),
+    routes: [
+        {
+            path: '/',
+            name: 'home',
+            component: MainPage
+        }
+    ]
+})
+
+export default router;
---
+++ src/views/MainPage.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+<main class="main">
+  <div class="container">
+
+  </div>
+</main>
+</template>
+
+<style>
+
+</style>
